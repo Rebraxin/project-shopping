2aa540a8d07412bfb229cc5889abd3a3f84534e7
Rebraxin <cedric.paje@gmail.com>
2020-10-05 14:22:48 +0200

    creating error handler middleware

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRouter from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -14,16 +16,16 @@ app.get('/', (req, res) => {
   res.send('API is running with nodemon...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRouter);
 
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use(notFound);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const ENV = process.env.NODE_ENV || 'Unknow';
 
-app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`.yellow.bold));
+app.listen(
+  PORT,
+  console.log(`Server running in ${ENV} mode on port ${PORT}`.yellow.bold)
+);
