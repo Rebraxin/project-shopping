64232733cb6a8a839143bb9400f61221d29daf58
Rebraxin <cedric.paje@gmail.com>
2020-10-05 13:10:02 +0200

    setup mongoose/mongodb/compass

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import products from './data/products.js';
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running with nodemon...');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/products/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+const PORT = process.env.PORT || 5000;
+const ENV = process.env.NODE_ENV || 'Unknow';
+
+app.listen(PORT, console.log(`Server running in ${ENV} mode on port ${PORT}`.yellow.bold));
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,24 +1,36 @@
 // == Import npm
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Col, Row } from 'react-bootstrap';
 
 // == Import local
 import Product from '../components/Product';
-import products from '../products';
 
 // == Component
-const HomeScreen = () => (
-  <>
-    <h1>Latest Products</h1>
-    <Row>
-      {products.map((product) => (
-        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-          <Product product={product} />
-        </Col>
-      ))}
-    </Row>
-  </>
-);
+const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((product) => (
+          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+            <Product product={product} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
 
 // == Export
 export default HomeScreen;
